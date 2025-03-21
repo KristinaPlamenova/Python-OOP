9fda62b8e681b73ff1d2a80f3fbff1037d59663a
Kristina Plamenova Krastinova <124092244+KristinaPlamenova@users.noreply.github.com>
2025-03-21 21:53:55 +0200

    Add files via upload

--- Vehicle.py
+++ Vehicle.py
@@ -1,12 +1,41 @@
-class Vehicle:
-    def __init__(self, mileage, max_speed=150):
-        self.max_speed = max_speed
-        self.mileage = mileage
-        self.gadgets = []
+from abc import ABC, abstractmethod
 
-car = Vehicle(20)
-print(car.max_speed)
-print(car.mileage)
-print(car.gadgets)
-car.gadgets.append('Hudly Wireless')
-print(car.gadgets)
+
+class Vehicle(ABC):
+    def __init__(self, fuel_quantity, fuel_consumption):
+        self.fuel_quantity = fuel_quantity
+        self.fuel_consumption = fuel_consumption
+
+    @abstractmethod
+    def drive(self, distance):
+        self.distance = distance
+        pass
+    @abstractmethod
+    def refuel(self, fuel):
+        self.fuel = fuel
+        pass
+
+class Car(Vehicle):
+    AC_CONSUMPTION = 0.9
+
+    def drive(self, distance):
+        fuel_needed = distance * (self.fuel_consumption + self.AC_CONSUMPTION)
+        if self.fuel_quantity >= fuel_needed:
+            self.fuel_quantity -= fuel_needed
+
+    def refuel(self, fuel):
+        self.fuel_quantity += fuel
+
+class Truck(Vehicle):
+    AC_CONSUMPTION = 1.6
+    FUEL_COEFFICIENT = 0.95
+
+    def drive(self, distance):
+        fuel_needed = distance * (self.fuel_consumption + self.AC_CONSUMPTION)
+        if self.fuel_quantity >= fuel_needed:
+            self.fuel_quantity -= fuel_needed
+
+
+    def refuel(self, fuel):
+        self.fuel_quantity += fuel * self.FUEL_COEFFICIENT
+
