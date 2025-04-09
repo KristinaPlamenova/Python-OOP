f07c0b672254c60867c0bc65b1d77c396730e04c
Kristina Plamenova Krastinova <124092244+KristinaPlamenova@users.noreply.github.com>
2025-04-09 15:00:56 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -1,52 +1,110 @@
 from unittest import TestCase, main
 
-from project.vehicle import Vehicle
+from project.hero import Hero
 
 
-class TestVehicle(TestCase):
-    fuel = 45.5
-    horse_power = 125.5
+class TestHero(TestCase):
+    username = "Test hero"
+    level = 5
+    health = 12.3
+    damage = 9.8
     def setUp(self):
-        self.test_vehicle = Vehicle(self.fuel, self.horse_power)
-
-    def test_class_attribute_types(self):
-        self.assertIsInstance(self.test_vehicle.DEFAULT_FUEL_CONSUMPTION, float)
-        self.assertIsInstance(self.test_vehicle.fuel_consumption, float)
-        self.assertIsInstance(self.test_vehicle.fuel, float)
-        self.assertIsInstance(self.test_vehicle.capacity, float)
-        self.assertIsInstance(self.test_vehicle.horse_power, float)
+        self.hero = Hero(self.username, self.level, self.health, self.damage)
 
     def test_init(self):
-        self.assertEqual(self.fuel, self.test_vehicle.fuel)
-        self.assertEqual(self.fuel, self.test_vehicle.capacity)
-        self.assertEqual(self.horse_power, self.test_vehicle.horse_power)
-        self.assertEqual(1.25, self.test_vehicle.fuel_consumption)
+        self.assertEqual(self.username, self.hero.username)
+        self.assertEqual(self.level, self.hero.level)
+        self.assertEqual(self.health, self.hero.health)
+        self.assertEqual(self.damage, self.hero.damage)
 
-    def test_drive_success(self):
-        self.test_vehicle.drive(20)
-        self.assertEqual(20.5, self.test_vehicle.fuel)
+    def test_attr_types(self):
+        self.assertIsInstance(self.hero.username, str)
+        self.assertIsInstance(self.hero.level, int)
+        self.assertIsInstance(self.hero.health, float)
+        self.assertIsInstance(self.hero.damage, float)
 
-    def test_drive_error_raises_ex(self):
+    def test_hero_enemy_same_names(self):
+        enemy = Hero(self.username, self.level, self.health, self.damage)
         with self.assertRaises(Exception) as ex:
-            self.test_vehicle.drive(3000)
-        self.assertEqual("Not enough fuel", str(ex.exception))
-        self.assertEqual(self.fuel, self.test_vehicle.fuel)
+            self.hero.battle(enemy)
+        self.assertEqual("You cannot fight yourself", str)
 
-    def test_refuel_success(self):
-        self.test_vehicle.fuel = 1
-        self.test_vehicle.refuel(30.5)
-        self.assertEqual(31.5, self.test_vehicle.fuel)
+    def test_hero_not_enough_health(self):
+        self.hero.health = 0
+        enemy = Hero("Enemy", self.level, self.health, self.damage)
 
-    def test_refuel_error_raises_ex(self):
-        self.test_vehicle.fuel = 40
-        with self.assertRaises(Exception) as ex:
-            self.test_vehicle.refuel(30)
-        self.assertEqual("Too much fuel", str(ex.exception))
+        with self.assertRaises(ValueError) as e:
+            self.hero.battle(enemy)
+        self.assertEqual("Your health is lower than or equal to 0. You need to rest", str(e.exception))
 
+        self.hero.health = -1
+        with self.assertRaises(ValueError) as e:
+            self.hero.battle(enemy)
+        self.assertEqual("Your health is lower than or equal to 0. You need to rest", str(e.exception))
+
+    def test_enemy_not_enough_health(self):
+        enemy = Hero("Enemy", self.level, 0, self.damage)
+        with self.assertRaises(ValueError) as e:
+            self.hero.battle(enemy)
+        self.assertEqual("fYou cannot fight Enemy. He needs to rest", str(e.exception))
+
+        enemy.health = -1
+        with self.assertRaises(ValueError) as e:
+            self.hero.battle(enemy)
+        self.assertEqual("fYou cannot fight Enemy. He needs to rest", str(e.exception))
+
+    def test_draw(self):
+        enemy = Hero("Enemy", self.level, self.health, self.damage)
+        result = self.hero.battle(enemy)
+        self.assertEqual(self.level, self.hero.level)
+        self.assertEqual(-36.7, self.hero.health)
+        self.assertEqual(self.damage, self.hero.damage)
+
+        self.assertEqual(self.level, enemy.level)
+        self.assertEqual(-36.7, enemy.health)
+        self.assertEqual(self.damage, enemy.damage)
+        self.assertEqual("Draw", result)
+
+    def test_hero_wins(self):
+        enemy = Hero("Enemy", 1, 1, 1)
+        result = self.hero.battle(enemy)
+        self.assertEqual(6, self.hero.level)
+        self.assertEqual(16.3, self.hero.health)
+        self.assertEqual(14.8, self.hero.damage)
+        self.assertEqual("You win", result)
+
+        self.assertEqual(1, enemy.level)
+        self.assertEqual(-48.0, enemy.health)
+        self.assertEqual(1, enemy.damage)
+
+    def test_hero_loses(self):
+        enemy = Hero("Enemy", 100, 100, 100)
+        result = self.hero.battle(enemy)
+        self.assertEqual(self.level, self.hero.level)
+        self.assertEqual(-9987.7, self.hero.health)
+        self.assertEqual(self.damage, self.hero.damage)
+        self.assertEqual("You lose", result)
+
+        self.assertEqual(101, self.hero.level)
+        self.assertEqual(56.0, self.hero.health)
+        self.assertEqual(105, self.hero.damage)
 
     def test_str(self):
-        expected = f"The vehicle has {self.horse_power} horse power with {self.fuel} fuel left and 1.25 fuel consumption"
-        self.assertEqual(expected, str(self.test_vehicle))
+        expected = f"Hero {self.username}: {self.level} lvl\n" \
+               f"Health: {self.health}\n" \
+               f"Damage: {self.damage}\n"
+
+        self.assertEqual(expected, str(self.hero))
+
+
+
+
+
+
+
+
+
+
 
 
 
